3d0aaf24bfa7d39a51fd16f4450ddcf02bd41531
tc89585 <tc89585@uga.edu>
2022-06-27 16:03:36 -0400

    tests passed

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -21,23 +21,103 @@ afterEach(() => {
 
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
+  //represents the components
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "04/28/2023";
+
+  //changing value of components
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  const check = screen.getAllByText(/Test/i);
+  expect(check.length).toBe(1);
  });
 
- test('test that App component doesn\'t add a task without task name', () => {
+test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+  //grab the components
+  const inputTaskElement = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDateElement = screen.getByPlaceholderText("mm/dd/yyyy");
+  const addButton = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "04/23/2023";
+
+  //fireEvent.change(inputTaskElement, {target: {}});
+  fireEvent.change(inputDateElement, {target: {value: dueDate}});
+  fireEvent.click(addButton);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+  
  });
 
- test('test that App component doesn\'t add a task without due date', () => {
+test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
+  //grab the components
+  const inputTaskElement = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDateElement = screen.getByPlaceholderText("mm/dd/yyyy");
+  const addButton = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "04/23/2023";
+
+  fireEvent.change(inputTaskElement, {target: {value: "Test"}});
+  //fireEvent.change(inputDateElement, {target: {value: dueDate}});
+  fireEvent.click(addButton);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
 
 
- test('test that App component can be deleted thru checkbox', () => {
+test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+   //grab the components
+   const inputTaskElement = screen.getByRole('textbox', {name: /Add New Item/i});
+   const inputDateElement = screen.getByPlaceholderText("mm/dd/yyyy");
+   const addButton = screen.getByRole('button', {name: /Add/i});
+   const dueDate = "04/23/2023";
+
+   fireEvent.change(inputTaskElement, {target: {value: "Test"}});
+   fireEvent.change(inputDateElement, {target: {value: dueDate}});
+   fireEvent.click(addButton);
+
+   const checkBoxElement = screen.getByRole('checkbox');
+   fireEvent.click(checkBoxElement);
+   const check = screen.getByText(/You have no todo's left/i);
+
+   expect(check).toBeInTheDocument();
+
  });
 
 
- test('test that App component renders different colors for past due events', () => {
+test('test that App component renders different colors for past due events', () => {
   render(<App />);
- });
+  const inputTaskElement = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDateElement = screen.getByPlaceholderText("mm/dd/yyyy");
+  const addButton = screen.getByRole('button', {name: /Add/i});
+
+  const validDueDate = "04/23/2023";
+  const pastDueDate = "04/23/2020";
+
+  //valid task
+  fireEvent.change(inputTaskElement, {target: {value: "ValidTask"}});
+  fireEvent.change(inputDateElement, {target: {value: validDueDate}});
+  fireEvent.click(addButton);
+
+  //invalid Task because past due date
+  fireEvent.change(inputTaskElement, {target: {value: "BadTask"}});
+  fireEvent.change(inputDateElement, {target: {value: pastDueDate}});
+  fireEvent.click(addButton);
+
+  const badTaskCheck = screen.getByTestId(/BadTask/i).style.backgroundColor;
+  const validTaskCheck = screen.getByTestId(/ValidTask/i).style.backgroundColor;
+  
+  expect(validTaskCheck === badTaskCheck).toBe(false);
+
+ }); 
--- src/component/todos.js
+++ src/component/todos.js
@@ -21,7 +21,7 @@ const Todos = ({ todos, deleteTodo }) => {
       return (
         <Grid key={todo.id}>
           <Card style={{marginTop:10, backgroundColor:color}}
-                data-test-id={todo.content}
+                data-testid={todo.content}
           >
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
